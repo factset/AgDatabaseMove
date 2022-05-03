07d4ed335b9b9f60a5bc112b752a702eca1eca47
Aditya Niraula <adityaniraula@gmail.com>
2022-05-03 17:07:18 +0100

    fix(backup-chain): next log backup can have the same `LastLsn`

--- src/BackupChain.cs
+++ src/BackupChain.cs
@@ -92,7 +92,7 @@ namespace AgDatabaseMove
     {
       // also gets all the stripes of the next backup
       return backups.Where(b => b.BackupType == BackupFileTools.BackupType.Log &&
-                                prevBackup.LastLsn >= b.FirstLsn && prevBackup.LastLsn + 1 < b.LastLsn);
+                                prevBackup.LastLsn >= b.FirstLsn && prevBackup.LastLsn <= b.LastLsn);
     }
 
     private static bool IsValidFilePath(BackupMetadata meta)
